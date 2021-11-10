a612c64f0d600487e55ff667a322f0d675dfe031
knaito-asial <knaito@asial.co.jp>
2021-11-10 14:31:50 +0900

    feat: to use firebase

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,3 @@
-const ncmb = new NCMB(applicationKey, clientKey);
-
 const $$ = Dom7;
 
 const app = new Framework7({
@@ -7,41 +5,15 @@ const app = new Framework7({
   name: 'framework7-core-tab-view', // App name
   theme: 'auto', // Automatic theme detection
 
-  // App root data
-  data: function () {
-    return {
-      ncmb: ncmb
-    }
-  },
   // App root methods
   methods: {
-    loadImage: async function(file) {
-      return new Promise((res, rej) => {
-        const fr = new FileReader;
-        fr.onload = (result) => {
-          res(fr.result);
-        }
-        fr.readAsDataURL(file);
-      });
-    },
-    updateWordCount: async function(wordBook) {
-      const Word = ncmb.DataStore('Word');
-      const words = await Word
-        .relatedTo(wordBook, 'words')
-        .equalTo('remember', false)
-        .count()
-        .fetchAll();
-      await wordBook
-        .set('words_count', words.count)
-        .update();
-    },
     getWords: async function(wordBook) {
-      const words = wordBook.words;
-      if (words == null) {
-        return [];
+        const words = wordBook.words;
+        if (words == null) {
+          return [];
+        }
+        return words;
       }
-      return words;
-    }
   },
   // App routes
   routes: routes,
